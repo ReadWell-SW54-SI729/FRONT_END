88b6af1787bafb6cf789c0d245298b0bfceae6a4
hernancit0 <ernanemiliom12@gmail.com>
2024-05-01 02:20:32 -0500

    Feat: add iniciosesion and register routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { BookDetailComponent } from './bookflow/components/book-detail/book-deta
 import { BookCardComponent} from "./bookflow/components/book-card/book-card.component";
 import {BookHomeComponent} from "./bookflow/components/book-home/book-home.component";
 import {BookCommentComponent} from "./bookflow/components/book-comment/book-comment.component";
+import {IniciosesionComponent} from "./bookflow/components/book-iniciosesion/iniciosesion/iniciosesion.component";
+import {RegistroComponent} from "./bookflow/components/book-registro/registro/registro.component";
 
 export const routes: Routes = [
   {path : 'home', component: BookHomeComponent},
   {path : 'home/Catalogue', component: BookCardComponent},
+  {path : 'home/iniciosesion', component: IniciosesionComponent},
+  {path : 'home/registro', component: RegistroComponent},
   {path: 'Catalogue/bookDetail/:id', component: BookDetailComponent },
   {path: 'Catalogue/bookDetail/:id/Comment', component: BookCommentComponent },
   {path : '', redirectTo: 'home', pathMatch: 'full'},
