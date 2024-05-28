1c159f362f92cff5e5db044a3c7b5a85941659fb
ern23zc <U202112907@upc.edu.pe>
2024-05-28 00:28:18 -0500

    feat User Sign In
    First version of User Signing In has been created. It includes its connection with an API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "./public/components/header/header.component";
 import {BookCardComponent} from "./bookflow/components/book-card/book-card.component";
 import {FooterComponent} from "./public/components/footer/footer.component";
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, BookCardComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, BookCardComponent, FooterComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/bookflow/components/book-registro/registro/registro.component.ts
+++ src/app/bookflow/components/book-registro/registro/registro.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import {RegistrarService} from "../../../services/registrar/registrar.service";
+import {FormsModule} from "@angular/forms";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.css'
 })
 export class RegistroComponent {
+  firstname: string = '';
+  lastname: string = '';
+  email: string = '';
+  password: string = '';
 
+  constructor(private postService: RegistrarService, private router: Router) { }
+
+  sendData() {
+
+    const data = {
+      firstname: this.firstname,
+      lastname: this.lastname,
+      email: this.email,
+      password: this.password
+    }
+    this.postService.postData(data).subscribe(response => {
+      console.log('Response from API:', response);
+      this.router.navigate(['/home']);
+    }, error => {
+      console.error('Error:', error);
+    });
+  }
 }
