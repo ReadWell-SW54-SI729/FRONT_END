6dc2cf2bac2ec1c3ad7de0e92fc44f6dbccfbd12
ern23zc <U202112907@upc.edu.pe>
2024-05-28 23:16:51 -0500

    feat Dialog Sign In
    A confirmation Dialog has been added

--- src/app/bookflow/components/book-registro/registro/registro.component.ts
+++ src/app/bookflow/components/book-registro/registro/registro.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import {RegistrarService} from "../../../services/registrar/registrar.service";
 import {FormsModule} from "@angular/forms";
 import { Router } from '@angular/router';
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-registro',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule, MatButtonModule, MatDialogModule
   ],
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.css'
@@ -18,10 +20,17 @@ export class RegistroComponent {
   email: string = '';
   password: string = '';
 
-  constructor(private postService: RegistrarService, private router: Router) { }
+  constructor(private postService: RegistrarService, private router: Router, public dialog: MatDialog) { }
+
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogContentExampleDialog);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 
   sendData() {
-
     const data = {
       firstname: this.firstname,
       lastname: this.lastname,
@@ -30,9 +39,18 @@ export class RegistroComponent {
     }
     this.postService.postData(data).subscribe(response => {
       console.log('Response from API:', response);
+      this.openDialog();
       this.router.navigate(['/home']);
     }, error => {
       console.error('Error:', error);
     });
   }
 }
+
+@Component({
+  selector: 'dialog.component',
+  templateUrl: '../../../../public/components/dialog/dialog/dialog.component.html',
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule],
+})
+export class DialogContentExampleDialog {}
