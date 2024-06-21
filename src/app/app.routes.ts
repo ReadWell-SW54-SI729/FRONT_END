7d6361dd73b0ed918b248e3560b84e816ab44d3e
ern23zc <U202112907@upc.edu.pe>
2024-06-21 00:39:42 -0500

    fix: Authors Catalogue Styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,19 @@
 import { Routes } from '@angular/router';
 import { BookDetailComponent } from './bookflow/components/book-detail/book-detail.component';
 import { BookCardComponent} from "./bookflow/components/book-card/book-card.component";
-import {BookHomeComponent} from "./bookflow/components/book-home/book-home.component";
-import {BookCommentComponent} from "./bookflow/components/book-comment/book-comment.component";
-import {IniciosesionComponent} from "./bookflow/components/book-iniciosesion/iniciosesion/iniciosesion.component";
-import {RegistroComponent} from "./bookflow/components/book-registro/registro/registro.component";
-import {BookTendenciaComponent} from "./bookflow/components/book-tendencia/book-tendencia.component";
-import {AutorComponent} from "./bookflow/components/book-listas/lista-autores/autor.component";
-import {LecturaComponent} from "./bookflow/components/book-listas/lista-lecturas/lectura.component";
-import {ProfileComponent} from "./bookflow/components/book-profile/profile/profile.component";
+import { BookHomeComponent} from "./bookflow/components/book-home/book-home.component";
+import { BookCommentComponent} from "./bookflow/components/book-comment/book-comment.component";
+import { IniciosesionComponent} from "./bookflow/components/book-iniciosesion/iniciosesion/iniciosesion.component";
+import { RegistroComponent} from "./bookflow/components/book-registro/registro/registro.component";
+import { BookTendenciaComponent} from "./bookflow/components/book-tendencia/book-tendencia.component";
+import { AutorComponent} from "./bookflow/components/book-listas/lista-autores/autor.component";
+import { LecturaComponent} from "./bookflow/components/book-listas/lista-lecturas/lectura.component";
+import { ProfileComponent} from "./bookflow/components/book-profile/profile/profile.component";
 import { BookEditprofileComponent } from "./bookflow/components/book-editprofile/book-editprofile.component";
-import {ReadingClubComponent} from "./bookflow/components/book-reading-club/reading-club/reading-club.component";
-import  {BookCreateRedingClubComponent} from "./bookflow/components/book-create-reding-club/book-create-reding-club.component";
+import { ReadingClubComponent} from "./bookflow/components/book-reading-club/reading-club/reading-club.component";
+import { BookCreateRedingClubComponent} from "./bookflow/components/book-create-reding-club/book-create-reding-club.component";
 import { BookUserClubComponent } from './bookflow/components/book-user-club/book-user-club.component';
-import {RecommendFriendsComponent} from "./bookflow/components/book-recommend/recommend-friends/recommend-friends.component";
+import { RecommendFriendsComponent} from "./bookflow/components/book-recommend/recommend-friends/recommend-friends.component";
 import { BookSubscriptionComponent } from './bookflow/components/book-subscription/book-subscription.component';
 import { PaymentDetailsComponent } from './bookflow/components/payment-details/payment-details.component';
 
@@ -24,11 +24,11 @@ export const routes: Routes = [
   {path : 'home/registro', component: RegistroComponent},
   {path : 'home/profile/editprofile', component: BookEditprofileComponent},
   {path : 'home/tendencia', component: BookTendenciaComponent},
-  {path: 'Catalogue/bookDetail/:id', component: BookDetailComponent },
-  {path: 'Catalogue/bookDetail/:id/Comment', component: BookCommentComponent },
-  {path: 'Catalogue/bookDetail/:id/Recommend', component: RecommendFriendsComponent },
-  {path: 'Catalogue/bookListLectures', component: LecturaComponent },
-  {path: 'Catalogue/bookListAuthors', component: AutorComponent },
+  {path : 'Catalogue/bookDetail/:id', component: BookDetailComponent },
+  {path : 'Catalogue/bookDetail/:id/Comment', component: BookCommentComponent },
+  {path : 'Catalogue/bookDetail/:id/Recommend', component: RecommendFriendsComponent },
+  {path : 'Catalogue/bookListLectures', component: LecturaComponent },
+  {path : 'Catalogue/bookListAuthors', component: AutorComponent },
   {path : 'Catalogue/reading-club', component: ReadingClubComponent},
   {path : 'Catalogue/reading-club/create-club', component: BookCreateRedingClubComponent},
   {path : 'profile/user-club', component: BookUserClubComponent},
