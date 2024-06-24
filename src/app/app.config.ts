a29d97567e3d4eb66602e637d0113be7138c3cb0
Paolo <u202219186@upc.edu.pe>
2024-06-24 16:58:28 -0500

    update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,5 +7,5 @@ import {provideHttpClient, withFetch} from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideAnimationsAsync(),
-  provideHttpClient(withFetch())]
+  provideHttpClient(withFetch()), provideAnimationsAsync()]
 };
